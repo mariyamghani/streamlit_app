6f479848d675a1cf570a48a15601acd343d8c1d9
Mariyam Ghani <56685290+mariyamghani@users.noreply.github.com>
2022-08-10 23:16:06 +0530

    Update app.py
    added new title, header, and text

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit
 
-streamlit.title('New App')
-streamlit.header('Menu')
-streamlit.text('content')
+streamlit.title('Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
