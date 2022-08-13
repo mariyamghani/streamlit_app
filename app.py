0e0eb5ddd9476e7013b4d3d34a089ddf783d879c
Mariyam Ghani <56685290+mariyamghani@users.noreply.github.com>
2022-08-13 22:32:05 +0530

    Update app.py
    normalize data and show as df

--- app.py
+++ app.py
@@ -24,4 +24,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())  #writes the data to the screen instead of showing response 200
+
+# takes json version of the response and normalizes it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# normalised data as a table
+streamlit.dataframe(fruityvice_normalized)
